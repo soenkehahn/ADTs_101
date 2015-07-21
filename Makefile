d763673f94fe3f46a68bb6456677da290c854b1c
Sönke Hahn <soenkehahn@gmail.com>
2015-07-21 15:19:54 +0800

    initial

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all:
+	doctest Slides.hs
---
+++ Slides.hs
@@ -0,0 +1,13 @@
+
+data Position
+  = Position Int Int
+
+-- $
+-- >>> :t Position
+-- Position :: Int -> Int -> Position
+
+-- $
+-- -- >>> :t x
+-- x :: Position -> Int
+
+main = return ()
